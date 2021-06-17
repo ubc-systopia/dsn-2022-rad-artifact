cca9897f522ed37ae40375c9a1aa4ca291cfc03a
Arpan Gujarati <arpanbg@mpi-sws.org>
2021-06-17 02:08:19 +0200

    Remove sys.path.append from the scripts

--- niraapad/middlebox/start_server.py
+++ niraapad/middlebox/start_server.py
@@ -7,19 +7,6 @@ file_path = os.path.dirname(os.path.abspath(__file__))
 
 # Path to the cps-security-code (aka project niraapad) git repo
 niraapad_path = os.path.dirname(os.path.dirname(file_path))
-sys.path.append(niraapad_path)
-
-# Path to the hein_robots git repo (submodule)
-hein_robots_path = os.path.join(niraapad_path, "hein_robots")
-sys.path.append(hein_robots_path)
-
-# Path to the urx git repo (submodule)
-urx_path = os.path.join(niraapad_path, "python-urx")
-sys.path.append(urx_path)
-
-# Path to the ftdi_serial git repo (submodule)
-ftdi_serial_path = os.path.join(niraapad_path, "ftdi_serial")
-sys.path.append(ftdi_serial_path)
 
 from niraapad.middlebox.niraapad_server import NiraapadServer
 
