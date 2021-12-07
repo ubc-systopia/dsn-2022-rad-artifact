a46556e094c320dae5a5fec1b12262af13cced5d
Zainab Saeed Wattoo <zswattoo@gmail.com>
2021-12-06 21:22:15 -0800

    Update exp_timeline.py

--- analysis/Dataset_CommandAnalysis/exp_timeline.py
+++ analysis/Dataset_CommandAnalysis/exp_timeline.py
@@ -33,12 +33,10 @@ def main():
             print(list(df))
 
             sns.stripplot(data=df, x="Timeline", y="Method_Name", jitter=False)
-            #plt.title("%s \n %s" % (item, df["Module"].unique()))
             plt.xlabel("Timestep", fontsize=15)
             plt.ylabel("")
             if item.startswith("20211013113911-1") or item.startswith("20211020125006-1") or item.startswith("20211018141513-1"):
                 plt.ylabel("Method Name", fontsize=15)
-            #plt.tight_layout()
             plt.savefig("./" + item.split(".csv")[0] + ".pdf" , bbox_inches="tight")
             plt.clf()
 
